7a79777a3036ce4f8ccd15cc676476b1786d616b
Davi Araujo <davi.amaral.araujo@gmail.com>
2025-04-01 10:15:48 -0300

    feat: initial commit

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,145 @@
+"use client"
+
+import * as React from "react"
+import {
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  Command,
+  Frame,
+  GalleryVerticalEnd,
+  Map,
+  PieChart,
+  Settings2,
+  SquareTerminal,
+} from "lucide-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavProjects } from "@/components/nav-projects"
+import { NavUser } from "@/components/nav-user"
+import { TeamSwitcher } from "@/components/team-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Acme Inc",
+      logo: GalleryVerticalEnd,
+      plan: "Enterprise",
+    },
+    {
+      name: "Acme Corp.",
+      logo: AudioWaveform,
+      plan: "Startup",
+    },
+    {
+      name: "Evil Corp.",
+      logo: Command,
+      plan: "Free",
+    },
+  ],
+  navMain: [
+    {
+      title: "Cultos",
+      url: "#",
+      icon: SquareTerminal,
+      isActive: true,
+      items: [
+        {
+          title: "Registro de Cultos",
+          url: "#",
+        },
+        {
+          title: "Métricas do Culto",
+          url: "#",
+        },
+        {
+          title: "Histórico de Cultos",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Celulas",
+      url: "#",
+      icon: Bot,
+      items: [
+        {
+          title: "Visão Geral",
+          url: "#",
+        },
+        {
+          title: "Lista de Celulas",
+          url: "#",
+        },
+        {
+          title: "Registro de Celula",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Escola Bíblica",
+      url: "#",
+      icon: BookOpen,
+      items: [
+        {
+          title: "Turmas",
+          url: "#",
+        },
+        {
+          title: "Alunos",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Batismos",
+      url: "#",
+      icon: Settings2,
+      items: [
+        {
+          title: "Cadastro de novo batismo",
+          url: "#",
+        },
+        {
+          title: "Histórico de batismos",
+          url: "#",
+        },
+        {
+          title: "Relatório de batismos",
+          url: "#",
+        },
+      ],
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
